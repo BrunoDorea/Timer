d82a119a7c06c62d37effdc5d5adc8a2dce4cc65
Bruno Henrique Félix Dórea <brunodorea@outlook.com.br>
2022-08-26 19:53:45 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var sec = 0
 var min = 0
+var hr = 0
 
 var interval
 
+function twoDigits(digit) {
+    if(digit < 10) {
+        return('0'+ digit)
+    } else {
+        return(digit)
+    }
+}
+
 function start() {
+    watch()
     interval = setInterval(watch, 1000)
 }
 
@@ -15,14 +25,18 @@ function stop() {
     clearInterval(interval)
     sec = 0
     min = 0
-    document.getElementById('watch').innerText='00:00'
+    document.getElementById('watch').innerText='00:00:00'
 }
 
 function watch() {
     sec++
-    if(sec==60) {
+    if(sec == 60) {
         min++
         sec = 0
+        if(min == 60){
+            min=0
+            hr++
+        }
     }
-    document.getElementById('watch').innerText=min +':'+ sec
+    document.getElementById('watch').innerText=twoDigits(hr) +':'+ twoDigits(min) +':'+ twoDigits(sec)
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 </h1>
 
 <p align="center">
-  <a href="#">Live Preview</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="https://timer-brunoh.netlify.app/">Live Preview</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#memo-licença">Licença</a>
